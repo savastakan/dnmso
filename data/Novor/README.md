8377a9652a77a0318ed6917d7b5a84ec7d32cd07
gokaybiz <gkymhmt@gmail.com>
2020-05-21 17:00:24 +0300

    update

---
+++ data/Novor/README.md
@@ -0,0 +1,15 @@
+## REQUIREMENTS
+- NodeJS
+
+## USAGE
+
+- Start terminal or console.
+- Type `npm install` to install dependencies.
+
+* To analyze Novor 1.05 results:
+    * `node success-rate-checker-105.js`
+
+* To analyze Novor 1.1beta results:
+    * `node success-rate-checker-11b.js`
+
+Outputs will be in output-files directory.
---
+++ data/Novor/success-rate-checker-105.js
@@ -0,0 +1,82 @@
+const inputFiles = {
+    novorinput: '../Novor1-05.csv',
+    mgfinput: '../1000spectra.mgf',
+}
+
+const outputFile = 'output-files/novor_1_05_match_rate.json'
+const outputFile2 = 'output-files/wo_parentheses_novor_1_05_match_rate.json'
+
+const { parse } = require('@fast-csv/parse')
+
+const fs = require('fs')
+const stream = require('stream')
+const readline = require('readline')
+
+const CSV_STRING = (fs.readFileSync(inputFiles.novorinput, { encoding: 'utf8' })).split('\n\n\n')[1]
+const MGF_SCAN_PATTERN = /seq=\{(.*?)\}(.*?)\nSCANS=(.*)/gm
+
+const instream = fs.createReadStream(inputFiles.mgfinput)
+const outstream = new stream
+const rl = readline.createInterface(instream, outstream)
+
+new Promise(res => {
+    let MGF_STRING = ''
+    rl.on('line', function (line) {
+        MGF_STRING += line + '\n'
+    });
+
+    rl.on('close', function () {
+        return res(MGF_STRING)
+    })
+}).then(mgf => {
+    let results = {}
+
+    let mgf_data;
+    while ((mgf_data = MGF_SCAN_PATTERN.exec(mgf)) !== null)
+        results[mgf_data[3]] = mgf_data[1]
+    let checker_output = []
+    let checker_wo_parentheses_output = []
+    const stream = parse({ headers: true })
+        .on('error', error => console.error(error))
+        .on('data', row => {
+            let r = {}
+            let rwop = {}
+            /* With parantheses */
+            r.scanNum = row[' scanNum'].trim()
+            r.novorPeptide = row[' peptide'].trim()
+            r.expectedPeptide = results[row[' scanNum'].trim()].trim()
+
+            if (results[r.scanNum].trim() == r.novorPeptide)
+                r.success = 1
+            else
+                r.success = 0
+            checker_output.push(r)
+
+            /* Without parantheses */
+            rwop.scanNum = row[' scanNum'].trim()
+            rwop.novorPeptide = row[' peptide'].trim().replace(/\([^)]*\)/g, '')
+            rwop.expectedPeptide = results[row[' scanNum'].trim()].trim()
+
+            if (results[rwop.scanNum].trim() == rwop.novorPeptide)
+                rwop.success = 1
+            else
+                rwop.success = 0
+            checker_wo_parentheses_output.push(rwop)
+        })
+        .on('end', () => {
+            fs.writeFileSync(outputFile, JSON.stringify(checker_output, null, 4))
+            fs.writeFileSync(`${outputFile2}`, JSON.stringify(checker_wo_parentheses_output, null, 4))
+            let countOfSuccess = checker_output.filter(row => row.success == 1).length
+            let countOfSuccessWOP = checker_wo_parentheses_output.filter(row => row.success == 1).length
+
+            console.log(`Used novor 1.05 version with default config.\nMatch rate is ${countOfSuccess}/${checker_output.length},\nMatch rate without parentheses [(Cam), (O)] is ${countOfSuccessWOP}/${checker_wo_parentheses_output.length}.\nYou should check '${outputFile}' and '${`wo_parentheses_${outputFile}`}' for more info.`)
+        }
+        )
+
+    stream.write(CSV_STRING)
+    stream.end()
+})
+
+
+
+
